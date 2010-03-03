e99659503ebf8c637599a18c590f8d4cca0cdadc
nathforge <github@nreynolds.me.uk>
2010-03-03 12:39:43 +0000

    Added setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
+
 setup(name='pydir',
       version='1.0',
       description='Pydir is mkdir for Python modules.',
       author='Nathan Reynolds',
       author_email='pypi@nreynolds.me.uk',
-      scripts=['pydir'])
-
+      scripts=['pydir'],
+      long_description=open('README.rst').read())
