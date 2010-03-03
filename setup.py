7d07905359f3cf7adf899b993373b778a83bba51
nathforge <github@nreynolds.me.uk>
2010-03-03 12:57:49 +0000

    Setup tweaks

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
 
-setup(name='pydir',
-      version='1.0',
-      description='Pydir is mkdir for Python modules.',
-      author='Nathan Reynolds',
-      author_email='pypi@nreynolds.me.uk',
-      scripts=['pydir'],
-      long_description=open('README.rst').read())
+setup(
+    name='pydir',
+    version='1.0',
+    description='Pydir is mkdir for Python modules.',
+    long_description=open('README.rst').read(),
+    author='Nathan Reynolds',
+    author_email='pypi@nreynolds.me.uk',
+    scripts=['pydir'],
+)
