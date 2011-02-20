b2624405e9bad9e2eb1406c8d5cde23787cd3927
Nathan Reynolds <nath@nreynolds.me.uk>
2011-02-20 16:14:57 +0000

    Updated README reference in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='pydir',
     version='1.01',
     description='Pydir is mkdir for Python modules',
-    long_description=open('README.rst').read(),
+    long_description=open('README.txt').read(),
     author='Nathan Reynolds',
     url='https://github.com/nathforge/pydir',
     zip_safe=True,
