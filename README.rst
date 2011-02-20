7e7463307cdb2ec53c469a84096c438368042dbb
Nathan Reynolds <nath@nreynolds.me.uk>
2011-02-20 16:12:12 +0000

    Tidy-up, and a fixed setup file

--- README.rst
+++ README.rst
@@ -1,16 +1,9 @@
-Pydir
-=====
+Pydir is mkdir for Python modules.
 
-Pydir is mkdir for Python modules::
-
+Example:
     $ pydir -v myproject/module/etc
     Created directory myproject/module/etc
     Created file myproject/__init__.py
     Created file myproject/module/__init__.py
     Created file myproject/module/etc/__init__.py
-    
-    $ pydir -v myproject/anothermodule
-    Created directory myproject/anothermodule
-    File already exists: myproject/__init__.py
-    Created file myproject/anothermodule/__init__.py
 
--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
+from setuptools import setup
 
 setup(
     name='pydir',
-    version='1.0',
-    description='Pydir is mkdir for Python modules.',
+    version='1.01',
+    description='Pydir is mkdir for Python modules',
     long_description=open('README.rst').read(),
     author='Nathan Reynolds',
-    author_email='pypi@nreynolds.me.uk',
-    scripts=['pydir'],
+    url='https://github.com/nathforge/pydir',
+    zip_safe=True,
+    entry_points = {
+        'console_scripts': [
+            'pydir=pydir:main',
+        ],
+    },
 )
+
